de038995373df27f78c9d3a92a463e42ee244d70
TKVogais <jgparanagua@gmail.com>
2025-11-04 20:35:00 -0300

    Alterando a porta padrão de 80 para 4000

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import { controllers } from "./api.express/list.controllers";
 import 'dotenv/config';
 
 const main = () => {
-    const PORT = Number(process.env.PORT) || 80
+    const PORT = Number(process.env.PORT) || 4000
     const HOST = process.env.HOSTAPI || "127.0.0.0"
     const apiExpress = ApiExpress.build();
     apiExpress.buildControllers(controllers);
